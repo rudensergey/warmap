1bccc7595a23ce0080f9faa5c9bf04df8de70836
Sergey Rudenko <srudenko@more.tv>
2022-03-02 16:56:25 +0400

    feat: add marker

--- src/components/_templates/Map/Map.tsx
+++ src/components/_templates/Map/Map.tsx
@@ -1,9 +1,11 @@
 // Absolute imports
-import React, { DetailedHTMLProps, HTMLAttributes, MouseEventHandler } from "react";
-import { Map, View } from "ol";
-import { Tile } from "ol/layer";
-import { XYZ } from "ol/source";
-import { transform } from "ol/proj";
+import React from "react";
+import { Map, View, Feature } from "ol";
+import { Tile, Vector as LayerVector } from "ol/layer";
+import { XYZ, Vector as SourceVector } from "ol/source";
+import { Style, Icon, Text, Fill, Stroke } from "ol/style";
+import { Geometry, Point } from "ol/geom";
+import { transform, fromLonLat } from "ol/proj";
 
 // Components
 import Button from "@shared/Button";
@@ -18,16 +20,18 @@ import { BUTTON_TYPE } from "@shared/Button/Button.types";
 import { NotificationContext } from "@shared/Notification/Notification";
 import { NOTIFICATION_TYPES, TShowNotification } from "@shared/Notification/Notification.types";
 
+let map: Map = null;
+
 class MapTemplate extends React.Component<{ showNotification: TShowNotification }, TMapState> {
-  private map: Map;
-  private coordinates: string;
+  private markerLayer: LayerVector<SourceVector<Geometry>>;
 
   constructor(props: { showNotification: TShowNotification }) {
     super(props);
 
     this.state = {
       map: null,
-      coordinates: null,
+      x: 0,
+      y: 0,
     };
 
     this.setCenter = this.setCenter.bind(this);
@@ -35,7 +39,7 @@ class MapTemplate extends React.Component<{ showNotification: TShowNotification 
   }
 
   componentDidMount(): void {
-    this.map = new Map({
+    map = new Map({
       target: "map",
       layers: [
         new Tile({
@@ -50,12 +54,12 @@ class MapTemplate extends React.Component<{ showNotification: TShowNotification 
     });
 
     this.centerMap(32.5947657, 49.4441132);
-    this.map.on("singleclick", this.onMapClick);
+    map.on("singleclick", this.onMapClick);
   }
 
   centerMap(long: number, lat: number) {
-    this.map.getView().setCenter(transform([long, lat], "EPSG:4326", "EPSG:3857"));
-    this.map.getView().setZoom(6);
+    map.getView().setCenter(transform([long, lat], "EPSG:4326", "EPSG:3857"));
+    map.getView().setZoom(6);
   }
 
   setCenter() {
@@ -63,7 +67,25 @@ class MapTemplate extends React.Component<{ showNotification: TShowNotification 
   }
 
   onMapClick(event) {
-    console.log(event.coordinate);
+    if (this.state.markerLayer) map.removeLayer(this.state.markerLayer);
+
+    this.props.showNotification("Координаты обновлены");
+
+    const marker = new Feature({
+      geometry: new Point(event.coordinate),
+      type: "accident-point",
+      name: "target-point",
+    });
+
+    const vectorLayer = new LayerVector({
+      source: new SourceVector({
+        features: [marker],
+      }),
+    });
+
+    map.addLayer(vectorLayer);
+
+    this.setState({ markerLayer: vectorLayer });
   }
 
   render() {
@@ -73,6 +95,7 @@ class MapTemplate extends React.Component<{ showNotification: TShowNotification 
           <p className={MAP.TITLE}>Украина: Карта преступлений против человечества</p>
           <Button onClick={this.setCenter}>Отцентровать</Button>
         </Menu>
+
         <div id="map" className="map"></div>
       </div>
     );
--- src/components/_templates/Map/Map.types.tsx
+++ src/components/_templates/Map/Map.types.tsx
@@ -1,12 +1,17 @@
-import { Map } from "ol";
+import { Geometry } from "ol/geom";
+import { Vector } from "ol/layer";
+import VectorSource from "ol/source/Vector";
 
 export type TMapState = Readonly<{
   map: null;
-  coordinates: null;
+  x: number;
+  y: number;
+  markerLayer?: Vector<VectorSource<Geometry>>;
 }>;
 
 export enum MAP {
   WRAPPER = "map__wrapper",
   TITLE = "map__title",
   BOX = "map__box",
+  PLACE = "map__place",
 }
